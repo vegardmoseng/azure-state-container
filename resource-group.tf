dacc2eadda2e20eb86150cc166d140bf928098a2
Super User <root@Vegard.>
2025-03-11 23:05:59 +0100

    resource group

--- resource-group.tf
+++ resource-group.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "tf-state-backend" {
-  name     = "azure-tf-state-backend"
+  name     = var.resource_group_name
   location = "Sweden Central"
   managed_by = "c3adec27-11c3-4c43-9d71-eb6b4aa64651"
 }
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "resource_group_name" {
+    type        = string
+    description = "The name of the resource group to create"
+    default     = null
+}
